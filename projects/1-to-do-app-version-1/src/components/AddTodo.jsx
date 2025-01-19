09a384fff65ec0ce825864a5c8e9e5aaccf6e573
yashmishracode <yashmishrawork9@gmail.com>
2025-01-19 11:05:46 +0530

    to-do version 1

--- projects/1-to-do-app-version-1/src/components/AddTodo.jsx
+++ projects/1-to-do-app-version-1/src/components/AddTodo.jsx
@@ -1,7 +1,7 @@
 function AddTodo() {
     return (
         <div className="container text-center">
-        <div className="row">
+        <div className="row kg-row">
           <div className="col-6">
             <input type="text" placeholder="Enter TO-DO Here" />
           </div>
@@ -9,7 +9,7 @@ function AddTodo() {
             <input type="date" />
           </div>
           <div className="col-2">
-            <button type="button" className="btn btn-success">
+            <button type="button" className="btn btn-success kg-button">
               Add
             </button>
           </div>
--- projects/1-to-do-app-version-1/src/components/TodoItem1.jsx
+++ projects/1-to-do-app-version-1/src/components/TodoItem1.jsx
@@ -3,11 +3,11 @@ function TodoItem1() {
     let todoDate = "2021-09-30";
   return (
     <div className="container ">
-      <div className="row">
+      <div className="row kg-row">
         <div className="col-6">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
-          <button type="button" className="btn btn-danger">
+          <button type="button" className="btn btn-danger kg-button">
             Delete
           </button>
         </div>
